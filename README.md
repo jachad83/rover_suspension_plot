078b56fda72237bd4329c17afdb764006d441eff
James Chadwick <james@j-chadwick.co.uk>
2018-10-25 17:48:54 +0100

    damped harmonic oscillation of spring given system variables from user

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ As part of The Open University Rover Challenge Team (http://www.ou-roverteam.co.
 
 One of the early stage tasks was to produce a model of how the suspension would behave in order to decide what components and material would be required in production.
 
-I originally completed the task using MatLab; this is a Python port of that task.
+I originally completed the task using MatLab; this is a Python port of that task and is essentially a model of an under-damped harmonic oscillator. 
 
 ## Version
 * 1.0.0
--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 # model variables
-INITIAL_DEFLECTION = 0.1  # in m
-MASS = 100  # in kg
-SPRING_LENGTH = 0.3  # in m
-SPRING_CONSTANT = 0.3
-DAMPENING = 0.3
 MIN_DEFLECTION = 0.01  # in m
-SAMPLE_RATE = 1000  # in ms
+SAMPLE_RATE = 0.01  # in s
+SAMPLE_TIME = 5  # in s
 
 
-def wave_calc():
-    # derive data
-    period = 0.01  # testing only
-    x = np.arange(SAMPLE_RATE)
-    y = np.cos((x * period)) * 0.1
+def wave_calc(deflection, mass, spring_constant, damping_coefficent):
+    # angular frequency
+    w = math.sqrt(spring_constant / mass)
+    print(f'Spring period of oscillation {math.sqrt(mass / (spring_constant*1000))*(2 * math.pi)} s.')
 
-    return x, y
+    # numpy arrays for x and y coords
+    x = np.arange(0, SAMPLE_TIME, SAMPLE_RATE)
+    y = np.arange(0, SAMPLE_TIME, SAMPLE_RATE)
+
+    # iterate over y array
+    with np.nditer(y, op_flags=['readwrite']) as arr:
+        for el in arr:
+            # under-damped harmonic oscillator equation
+            el[...] = deflection * math.exp(-el*damping_coefficent) * math.cos(2 * math.pi * el)
+
+    return wave_plot(x, y)
 
 
-def plot():
+def wave_plot(x, y):
     # plot data
-    x, y = wave_calc()
     plt.plot(x, y)
-    plt.xlabel('Time (ms)')
+    plt.title('Rover Suspension Displacement')
+    plt.xlabel('Time (s)')
     plt.ylabel('Displacement (m)')
+    plt.grid(True, which='both')
     plt.show()
 
 
 def main():
-    plot()
+    # user inputs
+    INITIAL_DEFLECTION = float(input('Initial deflection in m: '))
+    MASS = float(input('Mass in kg: '))
+    SPRING_CONSTANT = float(input('Spring constant in kiloNewtons per metre: '))
+    DAMPING_COEFFICIENT = float(input('Damping coefficient (unit-less): '))
+    wave_calc(deflection=INITIAL_DEFLECTION, mass=MASS, spring_constant=SPRING_CONSTANT, damping_coefficent=DAMPING_COEFFICIENT)
 
 
 if __name__ == "__main__":
